055904be1386dcb63cf29a033b4f440fe812c6e4
Carlos Fernandes <chfernandes01@hotmail.com>
2022-06-18 13:37:03 -0300

    Typing fix

--- utils/withSSRGuest.ts
+++ utils/withSSRGuest.ts
@@ -1,7 +1,7 @@
 import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult, GetStaticProps } from "next";
 import { parseCookies } from "nookies";
 
-export function withSSRGuest<P>(fn: GetServerSideProps<P>){
+export function withSSRGuest<P extends { [key: string]: any; }>(fn: GetServerSideProps<P>){
   return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
     const cookies = parseCookies(ctx);
 
